330b2a81862756f6ddec4168c98f07f01e2af4b8
Mulugeta Achiso <anem1351@gmail.com>
2022-06-28 10:30:56 -0700

    remove char aat n

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-	if n < 0:
-	   return str
 	part1 = str[:n]
 	part2 = str[n+1:]
-	return part1 + parts2
+	if n < 0:
+	    return str
+	else:
+	    return part1 + parts2
