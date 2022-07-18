be25b8e987ee1c90b7fb6f885bac249e80a3545e
Mulugeta Achiso <anem1351@gmail.com>
2022-07-18 09:10:54 -0700

    function from bytecode

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    cmpt = 0
-    for index in range(x):
+    count = 0
+    for a in range(0, x):
         try:
-            print('{:d}'.format(my_list[index]), end='')
-            cmpt += 1
-        except IndexError:
-            break
-        except Exception:
+            print("{:d}".format(my_list[a]), end="")
+            count = count + 1
+        except(ValueError, TypeError):
             pass
-    print('')
-    return cmpt
+    print("")
+    return count
