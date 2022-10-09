79c3edd811f545e6f94b0cef5773b52bcce12d2b
Mulugeta Achiso <anem1351@gmail.com>
2022-10-09 01:47:02 -0700

    7-model_state_fetch_all

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,36 +1,21 @@
 #!/usr/bin/python3
-"""cript that lists all State objects
-   from the database hbtn_0e_6_usa"""
-from sqlalchemy import (create_engine)
-from model_state import State, Base
-from sys import argv
+"""
+lists all State objects from a database
+"""
+
+import sqlalchemy
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
 
-
-def model_state():
-    """initializate function model_state for db"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1],
-        argv[2],
-        argv[3]),
-        pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    # associate it with our custom Session class
-    Session = sessionmaker()
-
-    # associate it with our custom Session class
-    Session.configure(bind=engine)
-
+if __name__ == "__main__":
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
-
-    rows = session.query(State).order_by(State.id).all()
-
-    for state in rows:
+    for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
-
     session.close()
-
-
-if __name__ == '__main__':
-    model_state()
