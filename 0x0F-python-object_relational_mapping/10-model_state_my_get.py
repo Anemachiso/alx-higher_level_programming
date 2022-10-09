0e12789ca02d582892410fd6e48857ceb0e88437
Mulugeta Achiso <anem1351@gmail.com>
2022-10-09 02:00:48 -0700

    10-model_state_my_get

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,46 +1,24 @@
 #!/usr/bin/python3
-"""script that prints the State object with the name
-   passed as argument from the database hbtn_0e_6_usa
 """
-from model_state import State, Base
+prints the State object with the name passed as argument from a database
+"""
+
+import sqlalchemy
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
-from sqlalchemy import create_engine
+from model_state import Base, State
 
-
-def model_state_my_get():
-    """my function that print state"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1],
-        argv[2],
-        argv[3]),
-        pool_pre_ping=True
-    )
-    Base.metadata.create_all(engine)
-
-    # associate it with our custom Session class
-    Session = sessionmaker()
-
-    Session.configure(bind=engine)
-
+if __name__ == "__main__":
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
-
-    rows = session.query(State)
-
-    name = argv[4]
-    found = False
-
-    for state in rows:
-        if state.name == name:
-            print("{}".format(state.id))
-            found = True
-            break
-
-    if found is False:
-            print("Not found")
-
+    state = session.query(State).filter_by(name=argv[4]).first()
+    if state is not None:
+        print(str(state.id))
+    else:
+        print("Not found")
     session.close()
-
-
-if __name__ == '__main__':
-    model_state_my_get()
