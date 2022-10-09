f389617b1166354cca92efd88d81cc66380ba860
Mulugeta Achiso <anem1351@gmail.com>
2022-10-09 02:14:06 -0700

    14-model_city_fetch_by_state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,33 +1,25 @@
 #!/usr/bin/python3
-"""Script that adds the State object Louisiana
-to the database 'hbtn_0e_6_usa"""
+"""
+prints all City objects from a database
+"""
 
-import sys
-from model_state import Base, State
-from model_city import City
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+from model_city import City
 
 
-def model_city_fetch_by_state():
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-
+if __name__ == "__main__":
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
-
-    query = session.query(City, State) \
-                   .filter(City.state_id == State.id) \
-                   .order_by(City.id)
-
-    for city, state in query:
+    rows = session.query(City, State).filter(City.state_id == State.id)\
+                                     .order_by(City.id).all()
+    for city, state in rows:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
-
     session.close()
-
-
-if __name__ == '__main__':
-    model_city_fetch_by_state()
