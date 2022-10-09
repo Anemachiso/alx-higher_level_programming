88716ccd9dcfe70b9efd8f9deb5ff8cb8b9e99ef
Mulugeta Achiso <anem1351@gmail.com>
2022-10-09 01:50:43 -0700

    8-model_state_fetch_first

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,39 +1,24 @@
 #!/usr/bin/python3
-"""script that prints the first State
-object from the database hbtn_0e_6_usa
 """
-from model_state import State, Base
-from sys import argv
-from sqlalchemy.orm import sessionmaker
+list the first State object from a database
+"""
+
+import sqlalchemy
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
 
-
-def model_state_first():
-    """create function"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1],
-        argv[2],
-        argv[3]),
-        pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    # associate it with our custom Session class
-    Session = sessionmaker()
-
-    # associate it with our custom Session class
-    Session.configure(bind=engine)
-
+if __name__ == "__main__":
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
-
-    rows = session.query(State).order_by(State.id).first()
-
-    if rows:
-        print('{}: {}'.format(rows.id, rows.name))
+    first_state = session.query(State).order_by(State.id).first()
+    if first_state is not None:
+        print("{}: {}".format(first_state.id, first_state.name))
     else:
         print("Nothing")
-
     session.close()
-
-
-if __name__ == '__main__':
-    model_state_first()
