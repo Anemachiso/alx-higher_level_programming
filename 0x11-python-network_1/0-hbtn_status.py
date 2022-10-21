831a3f95df83e402d5a5a13abf4346481769a63c
Mulugeta Achiso <anem1351@gmail.com>
2022-10-21 10:20:22 -0700

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,9 +8,8 @@ def hbtn_status_0():
     with urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         utf8 = html.decode('utf-8')
-        print("Body response:\n\t    - type: {}".format(type(html)))
-        print("\t    - content: {}\n\t    - utf8 content: {}".
-              format(html, utf8, end=""))
+        print("Body response:\n\t- type: {}".format(type(html)))
+        print("\t- content: {}\n\t- utf8 content: {}".format(html, utf8, end=""))
 
 
 if __name__ == '__main__':
