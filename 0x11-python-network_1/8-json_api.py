c6d0d61a47dbaa5f88bfc634e2f35404b1d68fd5
Mulugeta Achiso <anem1351@gmail.com>
2022-10-21 10:10:21 -0700

    json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -16,12 +16,12 @@ def json_api():
         payload = {'q': q}
         r = requests.post(url, payload)
         try:
-            json = r.json()
-            if len(json) == 0:
+            seart = r.json()
+            if len(seart) == 0:
                 print('No result')
             else:
-                json_id = json.get('id')
-                json_name = json.get('name')
+                json_id = seart.get('id')
+                json_name = seart.get('name')
                 print('[{}] {}'.format(json_id, json_name))
         except:
             print('Not a valid JSON')
