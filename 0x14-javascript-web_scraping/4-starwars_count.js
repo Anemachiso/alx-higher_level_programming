0ea3096d8d8ff60387e49c95ae6b973296f0e09b
Mulugeta Achiso <anem1351@gmail.com>
2022-11-15 04:00:08 -0800

    count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,15 +3,15 @@ const request = require('request');
 const apiUrl = process.argv[2];
 
 request.get(apiUrl, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
+  if (error)
+	console.log(error);
+  else {
     let countWedge = 0;
     const films = JSON.parse(body).results;
     for (let i = 0; i < films.length; i++) {
       const charList = films[i].characters;
       for (let j = 0; j < charList.length; j++) {
-        if (charList[j] === 'https://swapi-api.hbtn.io/api/films/18' || charList[j] === 'https://swapi-api.hbtn.io/api/films/18') {
+        if (charList[j] === 'https://swapi-api.hbtn.io/api/films/18/' || charList[j] === 'https://swapi-api.hbtn.io/api/films/18/') {
           countWedge += 1;
         }
       }
